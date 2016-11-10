a7c99300aec0f00c1bc9f178f64020be6057b349
madmarks <bratmiro@gmail.com>
2016-11-10 14:06:12 +0200

    Season class simplified

--- NBAStatistics.Models/Season.cs
+++ NBAStatistics.Models/Season.cs
@@ -5,25 +5,8 @@ namespace NBAStatistics.Models
 {
     public class Season
     {
-        private ICollection<PlayerSeasonPointsPerGame> players;
-
-        public Season()
-        {
-            this.players = new HashSet<PlayerSeasonPointsPerGame>();
-        }
-
         public int Id { get; set; }
 
         public string SeasonId { get; set; }
-
-        //public ushort StartYear { get; set; }
-
-        //public ushort EndYear { get; set; }
-
-        public virtual ICollection<PlayerSeasonPointsPerGame> Players
-        {
-            get { return this.players; }
-            set { this.players = value; }
-        }
     }
 }
